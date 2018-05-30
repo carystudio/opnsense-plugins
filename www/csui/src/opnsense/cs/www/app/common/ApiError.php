b3cbfd049b70c4183eebc93ae3ab9d5a156841d4
李富强 <lifuqiang@carystudio.com>
2018-05-30 14:56:31 +0800

    添加错误代码的信息类

---
+++ www/csui/src/opnsense/cs/www/app/common/ApiError.php
@@ -0,0 +1,18 @@
+<?php
+
+class ApiError
+{
+    const ERROR_ZH = array(
+        //通用错误
+        'ip_error'=>'IP不正确',
+        'netmask_error'=>'子网掩码不正确',
+        'gateway_error'=>'默认网关不正确',
+        'mac_error'=>'MAC地址不正确',
+        'interface_error'=>'接口不正确',
+        'protocol_error'=>'协议不正确',
+        'paramater_error'=>'参数错误',
+
+        //Network
+        ''=>''
+    );
+}
