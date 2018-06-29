41aa1f712e2dde883eae55df0e6b2a825c0a8731
柯建富 <kejianfu@carystudio.com>
2018-06-29 17:03:22 +0800

    添加APP通过cgi获取lanIp接口

--- www/csui/src/opnsense/cs/www/app/controllers/CgiController.php
+++ www/csui/src/opnsense/cs/www/app/controllers/CgiController.php
@@ -4,7 +4,14 @@ class CgiController extends BaseController
 {
     public function indexAction()
     {
-        echo "Cgi";
+        $result = array();
+        $text = $this->request->getRawBody();
+        $para = json_decode($text, true);
+        if("getSysStatusCfg" == $para['topicurl']){
+            $lanInfo = System::getLanIp();
+            $result = array('lanIp'=>$lanInfo['Ip']);
+        }
+        echo json_encode($result);
     }
 }
 
